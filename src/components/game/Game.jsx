af7fdb700f74e41abb2792b5b71093d22cf63ed0
Railton Barbosa <railtonbarbosaa@hotmail.com>
2022-07-11 22:59:28 -0300

    criado logica para verificar o campeão

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,22 +1,46 @@
 import styles from "./Game.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Icon from "../icon/Icon"
 
 import GameOption from "../gameOption/GameOption"
 
+const winnerTable = [
+ [0, 1, 2],
+ [3, 4, 5],
+ [6, 7, 8],
+ [0, 3, 6],
+ [1, 4, 7],
+ [2, 5, 8],
+ [0, 4, 8],
+ [2, 4, 6],
+]
+
 function Game () {
   const [gameState, steGameState] = useState (Array(9).fill(0)) //um array de nove posições começando em zero!
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(-1)
+  const [winner, setwinner] = useState(0)
 
   const handleClick = (pos) => {
-     if (gameState [pos] ===0) {
+     if (gameState [pos] ===0 && winner === 0) {
       let newGameState = [...gameState]
       newGameState [pos] = currentPlayer
-      setCurrentPlayer(currentPlayer * -1)
       steGameState(newGameState)
    }
      }
+
+     const verfyGame = () => {
+       winnerTable.forEach((line)=>{
+         const values = line.map ((pos) => gameState[pos])
+         const sum = values.reduce ((sum,value)=> sum + value)
+         if (sum === 3 || sum === -3) setwinner (sum / 3)
+       })
+     }
      
+     useEffect (() => {
+      setCurrentPlayer(currentPlayer * -1)
+      verfyGame ()
+     }, [gameState])
+
   return(
    <div className={styles.gameContent}>
     
