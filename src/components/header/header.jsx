98a11674f25306902406912896c60eeddf1f70a9
Railton Barbosa <railtonbarbosaa@hotmail.com>
2022-08-10 16:28:38 -0300

    teste.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ import Icon from "../icon/Icon"
 function Header () {
   return (
     <div className={styles.header}>
-      <Title>Jogo da velha</Title>
+      <Title>Jogo da velha.</Title>
       <Subtitle>criado por Railton Barbosa</Subtitle>
       <div className={styles.iconContent}>
         <Icon iconName="github" link="https://github.com/" size="30px" />
