614ab45ca71bacec2e50ff0cb510059a21d34aa5
Railton Barbosa <railtonbarbosaa@hotmail.com>
2022-10-10 21:33:53 -0300

    testando

---
+++ src/components/gameInfo/GameInfo.jsx
@@ -0,0 +1,49 @@
+import styles from './GameInfo.module.css'
+
+import Icon from '../icon/Icon'
+import Button from '../button/Button'
+
+function GameInfo ({ currentPlayer, winner, onReset, isDraw }) {
+
+  const shouldEnableButton = () => winner !== 0 || isDraw
+
+  return (
+    <div className={styles.gameInfo}>
+      {
+        !isDraw && winner === 0 &&
+          <>
+            <h4>Próximo a jogar:</h4>
+            {
+              currentPlayer === 1 && <Icon iconName="circle" />
+            }
+            {
+              currentPlayer === -1 && <Icon iconName="x" />
+            }
+          </>
+      }
+      {
+        !isDraw && winner !== 0 &&
+          <>
+            <h4>Fim de jogo! Campeão:</h4>
+            {
+              winner === 1 && <Icon iconName="circle" />
+            }
+            {
+              winner === -1 && <Icon iconName="x" />
+            }
+          </>
+      }
+      {
+        isDraw && <h4>Empate!</h4>
+      }
+      <Button
+        onClick={onReset}
+        disabled={!shouldEnableButton()}
+      >
+        Reiniciar
+      </Button>
+    </div>
+  )
+}
+
+export default GameInfo
